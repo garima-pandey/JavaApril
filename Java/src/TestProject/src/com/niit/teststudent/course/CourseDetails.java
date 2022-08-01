7fb1935a29ef34ae5503574dc5b2ad3f428b0f81
Garima Pandey <pgarima148@gmail.com>
2022-08-01 21:45:45 +0530

    Add files via upload

---
+++ Java/src/TestProject/src/com/niit/teststudent/course/CourseDetails.java
@@ -0,0 +1,19 @@
+package com.niit.teststudent.course;
+
+public class CourseDetails {
+	
+	private String name;
+
+	public CourseDetails() {
+		name = "Java";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ Java/src/TestProject/src/com/niit/teststudent/student/Student.java
@@ -0,0 +1,15 @@
+package com.niit.teststudent.student;
+
+import com.niit.teststudent.course.CourseDetails;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CourseDetails course = new CourseDetails();
+		System.out.println(course.getName());
+
+	}
+
+}
